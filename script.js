a7cd41716b7a94a0fc09d6683437a2eeb9524204
Devance Joshi <dj788@snu.edu.in>
2022-07-16 17:04:19 +0530

    [added cool features]

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
 const mainImg = document.querySelector('.main-img')
-const btn = document.querySelector('.rnd-btn')
+const btn = document.querySelector('.rnd')
 const search = document.querySelector('.search')
-const name = document.querySelector('#name')
+const theName = document.querySelector('#name')
 const baseURL = 'https://superheroapi.com/api.php/1207214223466233'
 
-search.addEventListener('click',findHero)
+search.addEventListener('click', findHero)
 
-function findHero(e){
-    let heroName = name.value
+const getHTML = (char) => {
+    const name = `<h2>${char.name}</h2>`
+    const stat = Object.keys(char.powerstats).map(stat => {
+        return `<h4>${stat.toUpperCase()}: ${char.powerstats[stat]}</h4>`
+
+    }).join('')
+    const img = `<img class="main-img" src="${char.image.url}"></img>`
+    mainImg.innerHTML = `${name}${img}${stat}`
+}
+
+function findHero(e) {
+    let heroName = theName.value
     // console.log(heroName)
     fetch(`${baseURL}/search/${heroName}`)
-    .then(response => response.json())
-    .then(json => {
-        console.log(json)
-        mainImg.innerHTML = `<img class="main-img" src="${json.results[0].image.url}">`
-    })
+        .then(response => response.json())
+        .then(json => {
+            const link = json.results[0]
+            getHTML(link)
+        })
 
 }
 
-btn.addEventListener('click',display)
+btn.addEventListener('click', display)
 
-function display(e){
-    
-    
-    
-    const superHero = (id) =>{
+function display(e) {
+    const superHero = (id) => {
         fetch(`${baseURL}/${id}`)
-        .then(response => response.json())
-        .then(json => {
-            console.log(json)
-            mainImg.innerHTML = `<img class="main-img" src="${json.image.url}">`
-            // console.log(`${json.image.url}`)
-        })
+            .then(response => response.json())
+            .then(json => {
+                getHTML(json)
+            })
     }
-    
-    const random = () =>{
-        result = Math.floor(Math.random()*731) + 1
+
+    const random = () => {
+        result = Math.floor(Math.random() * 731) + 1
         return result
     }
     superHero(random())
